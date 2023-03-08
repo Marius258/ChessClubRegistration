9024a74a00326e7d28ee73871554bb2720a42422
Marius258 <marius.kavaliauskas147@gmail.com>
2023-03-08 11:08:24 +0200

    initial files + mysql configuration  added

---
+++ backend/ChessClubRegistration/src/main/java/com/marius/lt/ChessClubRegistration/ChessClubRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.marius.lt.ChessClubRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessClubRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessClubRegistrationApplication.class, args);
+	}
+
+}
